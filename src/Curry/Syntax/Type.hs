123b5f3da12686a7a7dd500e34a1378a5a81fc9e
Björn Peemöller <bjp@informatik.uni-kiel.de>
2011-08-23 10:10:55 +0200

    TODO added

--- src/Curry/Syntax/Type.hs
+++ src/Curry/Syntax/Type.hs
@@ -17,6 +17,19 @@
     an unlimited range of integer constants in Curry programs.
 -}
 
+-- TODO: Extract the import declarations and move them to top-level:
+--
+-- data Module = Module ModuleIdent (Maybe ExportSpec) [ImportDecl] [Decl]
+--     deriving (Eq, Read, Show, Data, Typeable)
+--
+-- data ImportDecl = ImportDecl Position ModuleIdent Qualified
+--                              (Maybe ModuleIdent) (Maybe ImportSpec)
+--
+-- data Interface = Interface ModuleIdent [IImportDecl] [IDecl]
+--     deriving (Eq, Read, Show, Data, Typeable)
+--
+-- data IImportDecl = IImportDecl Position ModuleIdent
+
 {-# LANGUAGE DeriveDataTypeable #-}
 
 module Curry.Syntax.Type
@@ -39,21 +52,21 @@ import Curry.Base.Position
 -- ---------------------------------------------------------------------------
 
 data Module = Module ModuleIdent (Maybe ExportSpec) [Decl]
-    deriving (Eq, Show, Read, Data, Typeable)
+    deriving (Eq, Read, Show, Data, Typeable)
 
 -- ---------------------------------------------------------------------------
 -- Export specification
 -- ---------------------------------------------------------------------------
 
 data ExportSpec = Exporting Position [Export]
-    deriving (Eq, Show, Read, Data, Typeable)
+    deriving (Eq, Read, Show, Data, Typeable)
 
 data Export
   = Export         QualIdent         -- f/T
   | ExportTypeWith QualIdent [Ident] -- T(C1,...,Cn)
   | ExportTypeAll  QualIdent         -- T(..)
   | ExportModule   ModuleIdent       -- module M
-    deriving (Eq, Show, Read, Data, Typeable)
+    deriving (Eq, Read, Show, Data, Typeable)
 
 -- ---------------------------------------------------------------------------
 -- Module declarations
@@ -73,7 +86,7 @@ data Decl
   | FlatExternalDecl Position [Ident]
   | PatternDecl      Position ConstrTerm Rhs
   | ExtraVariables   Position [Ident]
-    deriving (Eq, Show, Read, Data, Typeable)
+    deriving (Eq, Read, Show, Data, Typeable)
 
 -- ---------------------------------------------------------------------------
 -- Import specification
@@ -84,13 +97,13 @@ type Qualified = Bool
 data ImportSpec
   = Importing Position [Import]
   | Hiding Position [Import]
-    deriving (Eq, Show, Read, Data, Typeable)
+    deriving (Eq, Read, Show, Data, Typeable)
 
 data Import
   = Import         Ident            -- f/T
   | ImportTypeWith Ident [Ident]    -- T(C1,...,Cn)
   | ImportTypeAll  Ident            -- T(..)
-    deriving (Eq, Show, Read, Data, Typeable)
+    deriving (Eq, Read, Show, Data, Typeable)
 
 -- ---------------------------------------------------------------------------
 -- Infix declaration
@@ -100,7 +113,7 @@ data Infix
   = InfixL
   | InfixR
   | Infix
-    deriving (Eq, Show, Read, Data, Typeable)
+    deriving (Eq, Read, Show, Data, Typeable)
 
 -- ---------------------------------------------------------------------------
 -- Data declaration
@@ -109,7 +122,7 @@ data Infix
 data ConstrDecl
   = ConstrDecl Position [Ident] Ident [TypeExpr]
   | ConOpDecl  Position [Ident] TypeExpr Ident TypeExpr
-    deriving (Eq, Show, Read, Data, Typeable)
+    deriving (Eq, Read, Show, Data, Typeable)
 
 -- ---------------------------------------------------------------------------
 -- Newtype declaration
@@ -117,17 +130,17 @@ data ConstrDecl
 
 data NewConstrDecl
  = NewConstrDecl Position [Ident] Ident TypeExpr
-   deriving (Eq, Show, Read, Data, Typeable)
+   deriving (Eq, Read, Show, Data, Typeable)
 
 data EvalAnnotation
   = EvalRigid
   | EvalChoice
-    deriving (Eq, Show, Read, Data, Typeable)
+    deriving (Eq, Read, Show, Data, Typeable)
 
 data CallConv
   = CallConvPrimitive
   | CallConvCCall
-    deriving (Eq, Show, Read, Data, Typeable)
+    deriving (Eq, Read, Show, Data, Typeable)
 
 -- | Module interfaces
 --
@@ -137,7 +150,7 @@ data CallConv
 -- correct FlatCurry function applications.
 
 data Interface = Interface ModuleIdent [IDecl]
-    deriving (Eq, Show, Read, Data, Typeable)
+    deriving (Eq, Read, Show, Data, Typeable)
 
 data IDecl
   = IImportDecl    Position ModuleIdent
@@ -147,7 +160,7 @@ data IDecl
   | INewtypeDecl   Position QualIdent [Ident] NewConstrDecl
   | ITypeDecl      Position QualIdent [Ident] TypeExpr
   | IFunctionDecl  Position QualIdent Int TypeExpr
-    deriving (Eq, Show, Read, Data, Typeable)
+    deriving (Eq, Read, Show, Data, Typeable)
 
 -- ---------------------------------------------------------------------------
 -- Types
@@ -161,43 +174,41 @@ data TypeExpr
   | ArrowType       TypeExpr TypeExpr
   | RecordType      [([Ident], TypeExpr)] (Maybe TypeExpr)
     -- {l1 :: t1,...,ln :: tn | r}
-    deriving (Eq, Show, Read, Data, Typeable)
+    deriving (Eq, Read, Show, Data, Typeable)
 
 -- ---------------------------------------------------------------------------
 -- Functions
 -- ---------------------------------------------------------------------------
 
 data Equation = Equation Position Lhs Rhs
-    deriving (Eq, Show, Read, Data, Typeable)
+    deriving (Eq, Read, Show, Data, Typeable)
 
 data Lhs
   = FunLhs Ident [ConstrTerm]
   | OpLhs  ConstrTerm Ident ConstrTerm
   | ApLhs  Lhs [ConstrTerm]
-    deriving (Eq, Show, Read, Data, Typeable)
+    deriving (Eq, Read, Show, Data, Typeable)
 
 data Rhs
   = SimpleRhs  Position Expression [Decl]
   | GuardedRhs [CondExpr] [Decl]
-    deriving (Eq, Show, Read, Data, Typeable)
+    deriving (Eq, Read, Show, Data, Typeable)
 
 data CondExpr = CondExpr Position Expression Expression
-    deriving (Eq, Show, Read, Data, Typeable)
+    deriving (Eq, Read, Show, Data, Typeable)
 
--- ---------------------------------------------------------------------------
--- \paragraph{Literals} The \texttt{Ident} argument of an \texttt{Int}
--- literal is used for supporting ad-hoc polymorphism on integer
--- numbers. An integer literal can be used either as an integer number or
--- as a floating-point number depending on its context. The compiler uses
--- the identifier of the \texttt{Int} literal for maintaining its type.
--- ---------------------------------------------------------------------------
-
+-- |Literals
+-- The 'Ident' argument of an @Int@ literal is used for supporting ad-hoc
+-- polymorphism on integer numbers. An integer literal can be used either as
+-- an integer number or as a floating-point number depending on its context.
+-- The compiler uses the identifier of the @Int@ literal for maintaining its
+-- type.
 data Literal
-  = Char   SrcRef Char        -- should be Int to handle Unicode
+  = Char   SrcRef Char
   | Int    Ident Integer
   | Float  SrcRef Double
-  | String SrcRef String    -- should be [Int] to handle Unicode
-    deriving (Eq, Show, Read, Data, Typeable)
+  | String SrcRef String
+    deriving (Eq, Read, Show, Data, Typeable)
 
 -- ---------------------------------------------------------------------------
 -- Patterns
@@ -218,7 +229,7 @@ data ConstrTerm
   | InfixFuncPattern   ConstrTerm QualIdent ConstrTerm
   | RecordPattern      [Field ConstrTerm] (Maybe ConstrTerm)
         -- {l1 = p1, ..., ln = pn}  oder {l1 = p1, ..., ln = pn | p}
-    deriving (Eq, Show, Read, Data, Typeable)
+    deriving (Eq, Read, Show, Data, Typeable)
 
 -- ---------------------------------------------------------------------------
 -- Expressions
@@ -248,44 +259,44 @@ data Expression
   | IfThenElse      SrcRef Expression Expression Expression
   | Case            SrcRef Expression [Alt]
   | RecordConstr    [Field Expression]            -- {l1 = e1,...,ln = en}
-  | RecordSelection Expression Ident           -- e -> l
+  | RecordSelection Expression Ident              -- e -> l
   | RecordUpdate    [Field Expression] Expression -- {l1 := e1,...,ln := en | e}
-    deriving (Eq, Show, Read, Data, Typeable)
+    deriving (Eq, Read, Show, Data, Typeable)
 
 data InfixOp
   = InfixOp     QualIdent
   | InfixConstr QualIdent
-    deriving (Eq, Show, Read, Data, Typeable)
+    deriving (Eq, Read, Show, Data, Typeable)
 
 data Statement
   = StmtExpr SrcRef Expression
   | StmtDecl [Decl]
   | StmtBind SrcRef ConstrTerm Expression
-    deriving (Eq, Show, Read, Data, Typeable)
+    deriving (Eq, Read, Show, Data, Typeable)
 
 data Alt = Alt Position ConstrTerm Rhs
-    deriving (Eq, Show, Read, Data, Typeable)
+    deriving (Eq, Read, Show, Data, Typeable)
 
 data Field a = Field Position Ident a
-    deriving (Eq, Show, Read, Data, Typeable)
+    deriving (Eq, Read, Show, Data, Typeable)
 
 instance SrcRefOf ConstrTerm where
-  srcRefOf (LiteralPattern l)       = srcRefOf l
-  srcRefOf (NegativePattern i _)    = srcRefOf i
-  srcRefOf (VariablePattern i)      = srcRefOf i
+  srcRefOf (LiteralPattern       l) = srcRefOf l
+  srcRefOf (NegativePattern    i _) = srcRefOf i
+  srcRefOf (VariablePattern      i) = srcRefOf i
   srcRefOf (ConstructorPattern i _) = srcRefOf i
-  srcRefOf (InfixPattern _ i _)     = srcRefOf i
-  srcRefOf (ParenPattern c)         = srcRefOf c
-  srcRefOf (TuplePattern s _)       = s
-  srcRefOf (ListPattern _ _)        = error "list pattern has several source refs"
-  srcRefOf (AsPattern i _)          = srcRefOf i
-  srcRefOf (LazyPattern s _)        = s
-  srcRefOf (FunctionPattern i _)    = srcRefOf i
+  srcRefOf (InfixPattern     _ i _) = srcRefOf i
+  srcRefOf (ParenPattern         c) = srcRefOf c
+  srcRefOf (TuplePattern       s _) = s
+  srcRefOf (ListPattern        _ _) = error "list pattern has several source refs"
+  srcRefOf (AsPattern          i _) = srcRefOf i
+  srcRefOf (LazyPattern        s _) = s
+  srcRefOf (FunctionPattern    i _) = srcRefOf i
   srcRefOf (InfixFuncPattern _ i _) = srcRefOf i
-  srcRefOf (RecordPattern _ _)      = error "record pattern has several source refs"
+  srcRefOf (RecordPattern      _ _) = error "record pattern has several source refs"
 
 instance SrcRefOf Literal where
-  srcRefOf (Char s   _) = s
-  srcRefOf (Int i    _) = srcRefOf i
-  srcRefOf (Float s  _) = s
+  srcRefOf (Char   s _) = s
+  srcRefOf (Int    i _) = srcRefOf i
+  srcRefOf (Float  s _) = s
   srcRefOf (String s _) = s
