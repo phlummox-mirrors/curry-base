216e5dbd219d91b398fce9f34b3959976cae92d3
Björn Peemöller <bjp@informatik.uni-kiel.de>
2014-08-07 12:30:06 +0200

    Improvements on equivalence checks

--- src/Curry/ExtendedFlat/InterfaceEquivalence.hs
+++ src/Curry/ExtendedFlat/InterfaceEquivalence.hs
@@ -24,36 +24,35 @@ eqInterface :: Prog -> Prog -> Bool
 eqInterface = (=~=)
 
 -- |Type class to express the equivalence of two values
-class IntfEquiv a where
+class Equiv a where
   (=~=) :: a -> a -> Bool
 
-instance IntfEquiv a => IntfEquiv [a] where
+instance Equiv a => Equiv [a] where
   []     =~= []     = True
   (x:xs) =~= (y:ys) = x =~= y && xs =~= ys
   _      =~= _      = False
 
-instance IntfEquiv Char where
-  (=~=) = (==)
+instance Equiv Char where (=~=) = (==)
 
 -- |Equivalence of lists independent of the order.
-eqvSet :: IntfEquiv a => [a] -> [a] -> Bool
+eqvSet :: Equiv a => [a] -> [a] -> Bool
 xs `eqvSet` ys = null (deleteFirstsBy (=~=) xs ys ++ deleteFirstsBy (=~=) ys xs)
 
-instance IntfEquiv Prog where
+instance Equiv Prog where
   Prog m1 is1 ts1 fs1 os1 =~= Prog m2 is2 ts2 fs2 os2
     = m1 == m2 && is1 `eqvSet` is2 && ts1 `eqvSet` ts2
                && fs1 `eqvSet` fs2 && os1 `eqvSet` os2
 
-instance IntfEquiv TypeDecl where (=~=) = (==)
+instance Equiv TypeDecl where (=~=) = (==)
 
-instance IntfEquiv FuncDecl where
+instance Equiv FuncDecl where
   Func qn1 ar1 vis1 ty1 r1 =~= Func qn2 ar2 vis2 ty2 r2
     = qn1 == qn2 && ar1 == ar2 && vis1 == vis2 && ty1 == ty2 && r1 =~= r2
 
 -- TODO: Check why arguments of rules are not checked for equivalence
-instance IntfEquiv Rule where
+instance Equiv Rule where
   Rule _ _   =~= Rule _ _   = True
   External _ =~= External _ = True
   _          =~= _          = False
 
-instance IntfEquiv OpDecl where (=~=) = (==)
+instance Equiv OpDecl where (=~=) = (==)
