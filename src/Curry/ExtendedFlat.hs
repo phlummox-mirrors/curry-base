7c89decec71a1bcc8bb6d4cab28e4207f7045a9a
Björn Peemöller <bjp@informatik.uni-kiel.de>
2015-07-09 14:55:56 +0200

    Added summary module for extended FlatCurry

---
+++ src/Curry/ExtendedFlat.hs
@@ -0,0 +1,18 @@
+{- |
+    Module      :  $Header$
+    Description :  Interface for reading and manipulating extended FlatCurry
+                   source code
+    Copyright   :  (c) 2015 Björn Peemöller
+    License     :  OtherLicense
+
+    Maintainer  :  bjp@informatik.uni-kiel.de
+    Stability   :  experimental
+    Portability :  portable
+-}
+module Curry.ExtendedFlat
+  ( module Curry.ExtendedFlat.Pretty
+  , module Curry.ExtendedFlat.Type
+  ) where
+
+import Curry.ExtendedFlat.Pretty
+import Curry.ExtendedFlat.Type
